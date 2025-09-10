902e540aa49502697a15e6b8f2b1c27368a7a2ac
은솔 <105327508+EUNSOLY@users.noreply.github.com>
2025-09-10 23:03:07 +0900

    250910 수정, 생성 페이지 레이아웃 구성 및 기본 hooks 생성

--- src/page/project/Form.tsx
+++ src/page/project/Form.tsx
@@ -1,8 +1,73 @@
+import type { ProjectsDataType } from '../../server/server';
+import { cn } from '../../util/tailwind-merge';
 import useFormPage from './hooks/useFormPage';
 
 const FormPage = () => {
   const hooks = useFormPage();
-  return <>폼영역</>;
+  return (
+    <form className={cn(`flex flex-col flex-1 gap-2.5`)}>
+      <div className={cn('flex items-center justify-between ')}>
+        <h3 className={cn('text-lg font-bold')}>프로젝트 {hooks.id ? '수정' : '생성'}</h3>
+        <button
+          type="submit"
+          className={cn(
+            `cursor-pointer min-w-[95px] h-[30px] flex items-center justify-center bg-[#2f68c5] text-white rounded-md font-bold`
+          )}
+        >
+          저장
+        </button>
+      </div>
+      <div className={cn(`overflow-auto bg-white rounded-lg border border-[#DEDEDE]  px-4 py-4 flex-1`)}>
+        <div className={cn('grid grid-cols-2 gap-4 ')}>
+          {hooks.inputList.map((input) => {
+            return (
+              <div className={cn(`flex flex-col min-w-[320px]`)} key={input.value}>
+                <label
+                  key={input.value}
+                  htmlFor={input.value}
+                  className={cn(`font-bold text-[#333] mb-2 text-sm`, 'flex items-center gap-2')}
+                >
+                  <span>{input.label}</span>
+                  {input.isRequired && <span className={cn(`text-[#e74c3c]`)}>*</span>}
+                </label>
+                <input
+                  type={input.type}
+                  id={input.value}
+                  name={input.value}
+                  placeholder={input.placeholder}
+                  className={cn(
+                    'bg-white border  border-[#e9ecef]',
+                    `px-4 py-3 text-sm  rounded-lg `,
+                    'focus:outline-none focus:border-[#2F68C5] '
+                  )}
+                  style={
+                    input.type === 'date'
+                      ? {
+                          colorScheme: 'light',
+                        }
+                      : undefined
+                  }
+                  value={hooks.formData[input.value as keyof ProjectsDataType] ?? ''}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => hooks.onChangeInput(e, input.type, input.value)}
+                  onClick={
+                    input.type === 'date'
+                      ? (e) => {
+                          const input = e.currentTarget as HTMLInputElement;
+                          if (typeof input.showPicker === 'function') {
+                            input.showPicker();
+                          }
+                        }
+                      : undefined
+                  }
+                  disabled={input.value === 'deptCode' || input.value === 'projectCode' ? true : false}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default FormPage;
--- src/page/project/hooks/useFormPage.tsx
+++ src/page/project/hooks/useFormPage.tsx
@@ -1,12 +1,90 @@
-import { useParams } from 'react-router-dom';
-import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
+import { getFindData, type ProjectsDataType } from '../../../server/server';
 
-interface hooks {}
+interface hooks {
+  id: string | null;
+  inputList: {
+    label: string;
+    value: string;
+    type: string;
+    isRequired: boolean;
+    placeholder: string;
+    defaultValue?: string;
+  }[];
+  formData: Partial<ProjectsDataType>;
+  onChangeInput: (e: React.ChangeEvent<HTMLInputElement>, type: string, key: string) => void;
+}
 const useFormPage = (): hooks => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const id = searchParams.get('id') || null; // 생성, 수정 체크값
+  const [searchParams] = useSearchParams();
+  const location = useLocation();
 
-  return {};
+  const id: string | null = searchParams.get('id') || null; // 생성, 수정 체크값
+  const [formData, setFormData] = useState<Partial<ProjectsDataType>>({});
+
+  const inputList = [
+    {
+      label: '부서코드',
+      value: 'deptCode',
+      type: 'text',
+      isRequired: true,
+      placeholder: '부서코드를 입력하세요.',
+      defaultValue: '',
+    },
+    {
+      label: '프로젝트코드',
+      value: 'projectCode',
+      type: 'text',
+      isRequired: true,
+      placeholder: '프로젝트코드를 입력하세요.',
+      defaultValue: '',
+    },
+    {
+      label: '프로젝트명',
+      value: 'projectName',
+      type: 'text',
+      isRequired: true,
+      placeholder: '프로젝트코드를 입력하세요.',
+    },
+    { label: '년도', value: 'year', type: 'text', isRequired: true, placeholder: '년도를 입력하세요. ex) 2024' },
+    { label: '시작일', value: 'startDate', type: 'date', isRequired: true, placeholder: '시작일을 선택해주세요.' },
+    { label: '종료일', value: 'endDate', type: 'date', isRequired: false, placeholder: '종료일을 선택해주세요.' },
+  ];
+
+  //input onchange
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>, type: string, key: string) => {
+    console.log(e, 'e');
+    console.log(e.target.value, 'e');
+    console.log(type, 'type');
+    setFormData({
+      ...formData,
+      [key]: e.target.value,
+    });
+  };
+
+  // 수정이라면 맞는 값 수정
+  const loadFormData = async (id: string) => {
+    const data = await getFindData<ProjectsDataType>('projects', id);
+    setFormData(data);
+  };
+
+  useEffect(() => {
+    if (id) {
+      loadFormData(id);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    if (!location.state) return;
+    setFormData({ ...formData, deptCode: location.state.deptCode, projectCode: location.state.projectCode });
+  }, [location.state]);
+
+  return {
+    id,
+    inputList,
+    formData,
+    onChangeInput,
+  };
 };
 
 export default useFormPage;
